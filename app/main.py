206754c9c6de486a67a5191eea25e7b2fa59d70e
Ashutosh Pandey <ashupandey1620@gmail.com>
2026-03-28 21:33:37 +0530

    middleware

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
-# app/main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes.chat import router
 
 app = FastAPI(title="AI Chatbot API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router, prefix="/api")
