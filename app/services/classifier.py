e5d4d2fa12325780413c05d8b4ff74fe0076b8b7
Ashutosh Pandey <ashupandey1620@gmail.com>
2026-03-29 00:11:37 +0530

    Final Changes

--- app/services/classifier.py
+++ app/services/classifier.py
@@ -4,7 +4,7 @@ import re
 def classify_message(msg: str):
     msg = msg.strip().lower()
 
-    if re.search(r"\b(\d+)\s*(hours|hrs|minutes|min)\b", msg):
+    if re.search(r"\b(\d+)\s*(hours|hrs|minutes|min|hour|minute|second|seconds|sec)\b", msg):
         return "deadline"
 
     if re.fullmatch(r"\d+", msg):
